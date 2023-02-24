d90cae6cf83416c7b64b09ee759f0c5e539898ff
Robert Gleason <l2ob122@gmail.com>
2023-02-23 21:18:33 -0800

    added qualifier beans to controllers

---
+++ src/main/java/com/robertgleason/spring6di/services/GreetingServicePropertyInjected.java
@@ -0,0 +1,1 @@
+package com.robertgleason.spring6di.services;import org.springframework.stereotype.Service;@Service("propertyGreetingService")public class GreetingServicePropertyInjected implements GreetingService {    @Override    public String sayGreeting() {        return "Friends dont let friend do propery injection";    }}
---
+++ src/main/java/com/robertgleason/spring6di/services/GreetingServiceSetterInjection.java
@@ -0,0 +1,1 @@
+package com.robertgleason.spring6di.services;import org.springframework.stereotype.Service;@Service("setterGreetingBean")public class GreetingServiceSetterInjection implements GreetingService {    @Override    public String sayGreeting() {        return "Hey I'm Setting a Greeting";    }}
